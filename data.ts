37db08aaa04f13067f7d2421b5124e6ee6e84a9f
hjhj9798-crypto <hjhj9798@gmail.com>
2026-06-10 12:03:57 +0900

    Update data.ts

--- data.ts
+++ data.ts
@@ -6,8 +6,8 @@ export const INITIAL_HOME: HomeData = {
   professionalTitle: '',
   description: '',
   heroVideoUrl: 'https://player.vimeo.com/video/1178017661',
-  portfolioPdfUrl: 'https://www.dropbox.com/scl/fi/wi6abe5c5dpe7pua5sse6/Portfolio.pdf?rlkey=b2ckxxogowfieroozl89vhhqk&st=dnlucil7&dl=0',
-  resumePdfUrl: 'https://www.dropbox.com/scl/fi/09t73uqgyhco1jbpryk2f/Resume.pdf?rlkey=uka6mrs2ioc8bixol9u6787bb&st=jth794gw&dl=0',
+  portfolioPdfUrl: 'https://github.com/hjhj9798-crypto/heeji-portfolio/raw/main/pdf/Portfolio.pdf',
+  resumePdfUrl: 'https://github.com/hjhj9798-crypto/heeji-portfolio/raw/main/pdf/Resume.pdf',
   backgroundGifUrl: 'https://www.dropbox.com/scl/fi/c9g92m7l9346rm10o6uqy/Home_Background.mp4?rlkey=t4tfcndi1mnzqoa6cy15mk1h4&raw=1'
 };
 
